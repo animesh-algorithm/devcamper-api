1d43fb9f38b97b09f9b72b3018d1ef516645d10a
Animesh Sharma <animesharma3@gmail.com>
2021-07-06 20:44:03 +0530

    Routes Controllers and Custom Middlewares

---
+++ controllers/bootcamps.js
@@ -0,0 +1,34 @@
+// @desc        Get ALL Bootcamps
+// @route       GET /api/v1/bootcamps
+// @access      Public
+exports.getBootcamps = (req, res, next) => {
+
+}
+
+// @desc        Get a single Bootcamp
+// @route       GET /api/v1/bootcamps/:id
+// @access      Public
+exports.getBootcamp = (req, res, next) => {
+
+}
+
+// @desc        Create a Bootcamp
+// @route       POST /api/v1/bootcamps
+// @access      Private
+exports.createBootcamp = (req, res, next) => {
+
+}
+
+// @desc        Update Bootcamp
+// @route       PUT /api/v1/bootcamps/:id
+// @access      Private
+exports.updateBootcamp = (req, res, next) => {
+
+}
+
+// @desc        Delete Bootcamps
+// @route       DELETE /api/v1/bootcamps/:id
+// @access      Private
+exports.deleteBootcamp = (req, res, next) => {
+
+}
