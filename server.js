4b8c1518bff0777c434f99ed7b3470195a31ab08
Animesh Sharma <animesharma3@gmail.com>
2021-07-07 14:02:34 +0530

    MongoDB Atlas Connection with mongoose

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const dotenv = require('dotenv')
 const bootcamps = require('./routes/bootcamps')
 // const logger = require('./middleware/logger')
 const morgan = require('morgan')
+const connectDB = require('./config/db.js')
 
 dotenv.config({
     path: './config/config.env'
 })
 
-const app = express()
+connectDB()
 
+const app = express()
 // app.use(logger)
 
 if (process.env.NODE_ENV === 'development') {
@@ -20,4 +22,12 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// Handle unhandled rejection
+// If database doesn't work we want our app to crash
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // Close server and exit process
+    server.close(() => process.exit(1))
+})
